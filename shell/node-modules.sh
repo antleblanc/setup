fdf3be4a22f898f5dd195a517321ac4e9fad7a51
Antoine Leblanc <ant.leblanc@gmail.com>
2019-11-24 21:54:31 +0100

    Update node-modules.sh

--- shell/node-modules.sh
+++ shell/node-modules.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-npm install --global npm@latest
-npm install --global trash-cli
-npm install --global empty-trash-cli
-npm install --global yo
-npm install --global bower
-npm install --global grunt-cli
-npm install --global gulp-cli
-npm install --global ava
-npm install --global xo
-npm install --global mocha
-npm install --global eslint
-npm install --global generator-generator
-npm install --global generator-webapp
-npm install --global generator-polymer
-npm install --global generator-electron
+npm install --global npm@latest \
+  trash-cli \
+  empty-trash-cli \
+  yo \
+  bower \
+  grunt-cli \
+  gulp-cli \
+  ava \
+  xo \
+  mocha \
+  eslint \
+  generator-generator \
+  generator-webapp \
+  generator-polymer \
+  generator-electron
